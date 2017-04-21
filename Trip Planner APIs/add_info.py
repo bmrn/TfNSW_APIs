b82d4451dfb0974367c4ecb34e4ce176ad436773
bmrn <ausben@gmail.com>
2017-04-21 11:04:22 +1000

    initial code for previously empty .py files

--- Trip Planner APIs/add_info.py
+++ Trip Planner APIs/add_info.py
@@ -1,1 +1,40 @@
 #Provides capability to display all public transport service status and incident information (as published from the Incident Capture System).
+from urllib.parse import urlencode
+import requests
+import json
+from pprint import pprint
+
+api_key = ""
+
+base_url = "https://api.transport.nsw.gov.au/v1/tp/"
+query_type = "add_info?"
+
+#initialise query param dictionary
+qdict = {}
+#add parameters
+qdict["outputFormat"] = "rapidJSON"
+qdict["filterDateValid"] = "21-04-2017"
+qdict["filterMOTType"] = "" #(1,4,5,7,9,11):(Train, LR, Bus, Coach, Ferry, School Bus)
+qdict["filterPublicationStatus"] = "current" #current or "" to include historical alerts
+qdict["itdLPxx_selStop"] = "" #filter by stop ID e.g. 10111010
+qdict["itdLPxx_selLine"] = "020T1" #filter by line no. e.g 020T1
+qdict["itdLPxx_selOperator"] = "" #filter by operatorid
+qdict["filterPNLineDir"] = "" #filter by route NNN:LLLLL:D, (NNN: subnet, LLLLL: Route number, D: direction H/R).
+qdict["filterPNLineSub"] = "" #filter by route NNN:LLLLL:E:D
+qdict["version"] = "10.2.1.15"
+
+#encode params as querystring
+qstring = urlencode(qdict)
+
+#buildurl
+urlsend = base_url + query_type + qstring
+
+#get authentication
+headers = {'Authorization': 'apikey ' + api_key, 'Accept': 'application/json'}
+response = requests.get(urlsend, headers=headers)
+
+#decode response and convert to JSON format
+respdict = json.loads(response.content.decode('utf-8'))
+
+#simple example to look at data
+pprint(respdict)
--- Trip Planner APIs/coord.py
+++ Trip Planner APIs/coord.py
@@ -1,1 +1,48 @@
 #When given a specific geographical location, this API finds public transport stops, stations, wharfs and points of interest around that location.
+
+from urllib.parse import urlencode
+import requests
+import json
+from pprint import pprint
+
+api_key = ""
+
+base_url = "https://api.transport.nsw.gov.au/v1/tp/"
+query_type = "coord?"
+
+#initialise query param dictionary
+qdict = {}
+#add parameters
+qdict["outputFormat"] = "rapidJSON"
+qdict["coord"] = "151.2:-33.9:EPSG:4326"
+qdict["coordOutputFormat"] = "EPSG:4326"
+qdict["inclFilter"] = 1
+qdict["type_1"] = "GIS_POINT"
+qdict["radius_1"] =1000
+qdict["inclDrawClasses_1"] = ""
+qdict["type_2"] = "BUS_POINT"
+qdict["radius_2"] =1000
+qdict["inclDrawClasses_2"] = ""
+qdict["type_3"] = "POI_POINT"
+qdict["radius_3"] =1000
+qdict["inclDrawClasses_3"] = ""
+qdict["purpose"] = ""
+
+qdict["version"] = "10.2.1.15"
+
+#encode params as querystring
+qstring = urlencode(qdict)
+
+#buildurl
+urlsend = base_url + query_type + qstring
+print(urlsend)
+
+#get authentication
+headers = {'Authorization': 'apikey ' + api_key, 'Accept': 'application/json'}
+response = requests.get(urlsend, headers=headers)
+
+#decode response and convert to JSON format
+respdict = json.loads(response.content.decode('utf-8'))
+
+#simple example to look at data
+pprint(respdict)
